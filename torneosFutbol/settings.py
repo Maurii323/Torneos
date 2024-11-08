2c4a035164251250d21f5e7e0d2c663aef595b27
Mauricio Arroyo <mauriii3233@gmail.com>
2024-11-07 22:22:32 -0300

    arreglo: database_url

--- torneosFutbol/settings.py
+++ torneosFutbol/settings.py
@@ -92,10 +92,7 @@ WSGI_APPLICATION = 'torneosFutbol.wsgi.application'
 if not DEBUG:
     DATABASES = {
         # lee las variables de entorno de la base de datos de render
-        'default': dj_database_url.config(
-            default='postgresql://postgres:postgres@localhost/postgres',    # Postgresql
-            conn_max_age=600
-        )
+        'default': dj_database_url.config(conn_max_age=600)
     }
 else:
     DATABASES = {
